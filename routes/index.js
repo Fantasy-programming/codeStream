7e12fca5118a0fd1a51a4062878b4e2aeca3f56c
gamernewone <37631123+gamernewone@users.noreply.github.com>
2023-05-07 17:17:06 +0000

    Create about routes & add navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Codeshare, Pair programming platform' });
+});
+
+router.get("/about", function(req, res, next) {
+  res.render('about', { title: 'Codeshare, pair programming platform' });
 });
 
 module.exports = router;
