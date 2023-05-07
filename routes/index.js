124934511be1f359a58f4e1d8613c103741aff30
gamernewone <37631123+gamernewone@users.noreply.github.com>
2023-05-07 19:24:09 +0000

    Create the contact us page/routes

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Codeshare, Pair programming platform' });
 });
 
-router.get("/about", function(req, res, next) {
+router.get("/about", function (req, res, next) {
   res.render('about', { title: 'Codeshare, pair programming platform' });
 });
+
+router.route("/contact")
+  .get(function (req, res, next) {
+    res.render('contact', { title: 'Codeshare, pair programming platform' });
+  })
+  .post(function (req, res, next) {
+    res.render('thank', { title: 'Codeshare, pair programming platform' })
+  });
 
 module.exports = router;
